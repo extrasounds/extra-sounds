354dda3e370a0f8d268ed8748631d3cb320c06d5
lonefelidae16 <kow161206@gmail.com>
2026-06-13 13:07:11 +0900

    Prepare VersionedText

--- logics/src/main/java/dev/stashy/extrasounds/logics/impl/ChatSoundHandler.java
+++ logics/src/main/java/dev/stashy/extrasounds/logics/impl/ChatSoundHandler.java
@@ -1,10 +1,10 @@
 package dev.stashy.extrasounds.logics.impl;
 
 import dev.stashy.extrasounds.logics.ExtraSounds;
+import dev.stashy.extrasounds.logics.runtime.VersionedText;
 import dev.stashy.extrasounds.sounds.SoundType;
 import dev.stashy.extrasounds.sounds.Sounds;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
 
 import java.util.Objects;
 
@@ -13,7 +13,7 @@ public final class ChatSoundHandler {
 
     public void onMessage(PlayerEntity player, String text) {
         boolean containsPlName = false;
-        boolean containsScreenshot = text.matches(Text.translatable("screenshot.success", ".+?").getString());
+        boolean containsScreenshot = text.matches(VersionedText.INSTANCE.translatable("screenshot.success", ".+?").getString());
         try {
             containsPlName |= text.contains("@" + player.getName().getString());
             containsPlName |= text.contains("@" + Objects.requireNonNull(player.getDisplayName()).getString());
